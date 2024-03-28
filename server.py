0c9b635f0a30d27d597fe83daa7caacdbd69e50a
Johny-Robit <jean.robitaille.2@ulaval.ca>
2024-03-28 18:41:51 -0400

    Ajout d'une gâchette pour empêcher l'insertion d'une deuxième racine dans la table Categorie.
    Ajout d'une restriction de délétion d'une catégorie qui a des enfants dans la table Categorie.
    
    Ajout d'une fonctionnalité d'inscription

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, Response, request
+import pymysql
+from flask import Flask, render_template, Response, request, url_for, redirect
+import bcrypt
 from database import Database
 
 app = Flask(__name__)
@@ -8,6 +10,46 @@ database = Database()
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/inscription", methods=["GET", "POST"])
+def inscription():
+    if request.method == "POST":
+        # récupérer les données de la form
+        nom = request.form.get("nomUtilisateur")
+        email = request.form.get("email")
+        genre = request.form.get("choixGenre")
+        # avoir un choix d'utilisateur sous format 1 pour modérateur, 0 pour utilisateur
+        choixTypeUtilisateur = request.form.get("choixTypeUtilisateur")
+        typeUtilisateur = 1 if choixTypeUtilisateur == 'moderateur' else 0
+        # hasher le mot de passe de l'utilisateur
+        motDePasseForm = request.form.get("motDePasse")
+        motPasseEnBytes = motDePasseForm.encode('utf-8')
+        motDePasseHash = bcrypt.hashpw(motPasseEnBytes, bcrypt.gensalt())
+
+        # ouvrir une connection
+        connection = database.get_connection()
+        cursor = connection.cursor()
+
+        # Préparer la requête SQL
+        requete = "INSERT INTO utilisateurs (nom, motDePasse, email, genre, role) VALUES (%s, %s, %s, %s, %s)"
+        data = (nom, motDePasseHash, email, genre, typeUtilisateur)
+
+        # Envoyer la requete
+        try:
+            cursor.execute(requete, data)
+            connection.commit()
+            return redirect(url_for('index') + '?message=Inscription réussie !')
+
+        except pymysql.MySQLError as e:
+            print(e)
+            connection.rollback()
+            return redirect(url_for('inscription') + f'?message=Erreur à l inscription ${e}')
+
+        finally:
+            cursor.close()
+    else:
+        return render_template('inscription.html')
 
 
 @app.route("/admin")
